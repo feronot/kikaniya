0e44ec37991d7469e49a2879f952389077ae4e62
karenjitleone <karenjitleone@telegram.com>
2022-05-14 13:48:40 +0530

    Telegran @sikanndar_bot

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,47 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = "start9"
+        self.MirrorCommand = "mirror9"
+        self.UnzipMirrorCommand = "unzipmirror9"
+        self.ZipMirrorCommand = "zipmirror9"
+        self.TarMirrorCommand = "tarmirror9"
+        self.QbMirrorCommand = 'qbmirror9'
+        self.QbUnzipMirrorCommand = 'qbunzipmirror9'
+        self.QbZipMirrorCommand = 'qbzipmirror9'
+        self.QbTarMirrorCommand = 'qbtarmirror9'
+        self.CancelMirror = "cancel9"
+        self.CancelAllCommand = "cancelall9"
+        self.ListCommand = "list9"
+        self.StatusCommand = "status9"
+        self.AuthorizeCommand = "authorize"
+        self.UnAuthorizeCommand = "unauthorize"
+        self.AuthorizedUsersCommand = 'users'
+        self.AddSudoCommand = 'addsudo'
+        self.RmSudoCommand = 'rmsudo'
+        self.PingCommand = "ping9"
+        self.RestartCommand = "restart9"
+        self.StatsCommand = "stats9"
+        self.HelpCommand = "help9"
+        self.LogCommand = "log9"
+        self.CloneCommand = "clone9"
+        self.WatchCommand = "watch9"
+        self.ZipWatchCommand = "zipwatch9"
+        self.TarWatchCommand = "tarwatch9"
+        self.deleteCommand = "del9"
+        self.LeechSetCommand = 'leechset9'
+        self.SetThumbCommand = 'setthumb9'
+        self.LeechCommand = 'leech9'
+        self.TarLeechCommand = 'tarleech9'
+        self.UnzipLeechCommand = 'unzipleech9'
+        self.ZipLeechCommand = 'zipleech9'
+        self.QbLeechCommand = 'qbleech9'
+        self.QbUnzipLeechCommand = 'qbunzipleech9'
+        self.QbZipLeechCommand = 'qbzipleech9'
+        self.QbTarLeechCommand = 'qbtarleech9'
+        self.LeechWatchCommand = 'leechwatch9'
+        self.LeechTarWatchCommand = 'leechtarwatch9'
+        self.LeechZipWatchCommand = 'leechzipwatch9'
+        self.CountCommand = "count9"
+        self.SpeedCommand = "speedtest9"
+
+BotCommands = _BotCommands()
